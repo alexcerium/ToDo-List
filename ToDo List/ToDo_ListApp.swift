5c89e31b47217eb0fd349972fa03e8e801d49a71
alexcerium <141295618+alexcerium@users.noreply.github.com>
2025-02-06 17:45:30 +0300

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Aleksandr on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
